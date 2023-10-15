fcc9bbdf159945db839e85ee2d81fce83373f5ff
Matheus Loureiro <loureiro58@gmail.com>
2023-10-14 23:03:14 -0300

    Página de detalhes da notícia e passagem de conteúdo dinâmico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://www.zup.com.br/wp-content/uploads/2021/03/5e32dc5538c14b0d4b28f87f_imagens-angular3-1024x677.png",
+    "title":"Saiu uma nova versão do angular",
+    "description": "Novidades de otimização de performance no angular apontam tendências de mercado."
+  },
+  {
+    "id":"3",
+    "photo":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+    "title":"Filmes da Marvel tem aceitação acima de 80% no público geral",
+    "description": "Os filmes da Marvel tem agradado grande parte da população nos últimos 10 anos."
+  },
+  {
+    "id":"4",
+    "photo":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/BC405E86D7D358CF674490B9B9EAC46412DBC49D556BCD396100C000C216D173/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "title":"Timão e Pumba, uma nova aventura",
+    "description": "Estréia nova temporada de Timão e Pumba, entitulada de o Leão e sua Juba"
+  },
+  {
+    "id":"5",
+    "photo":"https://roadiecrew.com/wp-content/uploads/hammerfallmay2019band_638.jpg",
+    "title":"HammerFall com novos integrantes",
+    "description": "Haverá show no Brasil com novos integrantes da banda HammerFall.."
+  }
+
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent  implements OnInit{
+  @Input()
+  photoCover:string = "";
+
+  @Input()
+  cardTitle: string = "";
+
+  @Input()
+  cardDescription: string = "";
+
+  private id:string | null = "0";
+
+  constructor(
+    private route:ActivatedRoute
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id:string | null){
+     const result = dataFake.filter(article => article.id == id)[0]
+
+     this.cardTitle = result.title
+     this.cardDescription = result.description
+     this.photoCover = result.photo
+
+  }
+
+}
